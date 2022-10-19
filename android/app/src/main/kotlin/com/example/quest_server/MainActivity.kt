face4d37507a846d54404ce7af3eadbb6ea0ead2
BuildTools <unconfigured@null.spigotmc.org>
2022-10-19 16:11:59 +0530

    deleted all files

--- android/app/src/main/kotlin/com/example/quest_server/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.quest_server
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- lib/app/provider/app.provider.dart
+++
@@ -1,9 +0,0 @@
-import 'package:provider/provider.dart';
-import 'package:provider/single_child_widget.dart';
-import 'package:quest_server/core/notifier/authentication.notifier.dart';
-
-class AppProviders {
-  static List<SingleChildWidget> providers = [
-    ChangeNotifierProvider(create: (_) => AuthenticationNotifier())
-  ];
-}
